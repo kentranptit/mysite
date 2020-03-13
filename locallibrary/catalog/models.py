7bf7833794ea40c68e7bd0dc48c99b2124608d31
kentranptit <kentran24797@gmail.com>
2020-03-13 11:41:05 +0700

    Part 8: User authentication and permissions

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse   # Used to generate URLs by reversing the URL patterns
 import uuid   # Required for unique book instances
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Genre(models.Model):
@@ -52,6 +54,7 @@ class BookInstance(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
   book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
   imprint = models.CharField(max_length=200)
+  borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
   due_back = models.DateField(null=True, blank=True)
 
   LOAN_STATUS = (
@@ -71,6 +74,13 @@ class BookInstance(models.Model):
 
   class Meta:
     ordering = ['due_back']
+    permissions = (("can_mark_returned", "Set book as returned"),)
+  
+  @property
+  def is_overdue(self):
+    if self.due_back and date.today() > self.due_back:
+      return True
+    return False
 
   def __str__(self):
     return f'{self.id} ({self.book.title})'
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 def index(request):
@@ -50,3 +51,22 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-based view listing books on loan to current user"""
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 2
+    
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class AllLoanedBooksListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 2
+
+    permission_required = 'catalog.can_mark_returned'
+    
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
